05e25f137b22f51a8eeabda1e5fbe460af135b0b
unknown <egorvasiliev1997@mail.ru>
2024-04-02 22:58:18 +0700

    First commit

---
+++ Main.cs
@@ -0,0 +1,15 @@
+﻿namespace MainApp{}
+
+internal class Mains
+{
+   static void Main(string[] args)
+   {
+      Console.WriteLine("\nДобро пожаловать в приложение управление учениками\n\n");
+      Console.WriteLine("Что вы хотете сделать?:");
+      Console.WriteLine("1. Просмотреть информацию об учениках");
+      Console.WriteLine("2. Добавить ученика");
+      Console.WriteLine("3. Редактировать ученика");
+      Console.WriteLine("4. Найти ученика по ФИО");
+      Console.WriteLine("5. Удалить ученика");
+   }
+}
